de806315e292934e762f4ded7aa293a89d1f6416
BhargaviChada <bhargavichada2013@gmail.com>
2021-12-18 19:23:44 +0530

    Added tests, deployed campaignFactory contract

---
+++ ethereum/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+
+const provider = new HDWalletProvider(
+  "recycle help three dose female mystery teach large plate soon zone mango",
+  "wss://goerli.infura.io/ws/v3/3101da5cdc644a2fa2a24cec201bf011"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
